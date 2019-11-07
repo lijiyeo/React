b5bbd701ca795fbe0aa9c6d820fe768054fa1cd4
lijiyeo <39698638+lijiyeo@users.noreply.github.com>
2019-11-07 13:41:28 +0800

    Add files via upload

---
+++ cnode/src/Components/Right.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default function Right(){
+    return (
+        <div>
+            <div className="right">
+                <h3>CNode：Node.js专业中文社区</h3>
+                <p>您可以 登录 或 注册，也可以</p>
+                <button>通过 github 登录</button>
+            </div>
+            
+            <img style={{width:"270px"}} src="https://mfiles.alphacoders.com/639/639353.jpg"/>
+        </div>                 
+    )
+}
